de5aa1ec0b44976d95837667ab8c36b9820bc3a1
Exequiel <exequiel.segura@hotmail.com>
2021-04-14 03:42:15 -0300

    Ya anda todo, esta terminado el ascensor

--- js/Ascensor.js
+++ js/Ascensor.js
@@ -2,18 +2,51 @@ class Ascensor {
     cantidadPisos;
     pisoActual;
     filaLlamadas;
+    moviendose;
 
     constructor (cantidadPisos) {
         this.cantidadPisos = cantidadPisos;
         this.pisoActual = 0; 
-        this.filaLlamadas = [5, 2, 0, 3, 4]; 
+        this.filaLlamadas = [0]; //Si no seteas un piso tepea y se queda prendido el primer boton que toques.??
+        this.moviendose = false;
+        
     }
 
-    moverAscensor () {
+    dibujarAscensor() {
         var ascensor = document.getElementById('ascensor');
         var siguientePiso = this.filaLlamadas[0];
         var posicion = (this.cantidadPisos * 100) - (siguientePiso * 100) - 100; 
         ascensor.style.top = `${posicion}px`;
+    }
+
+    moverAscensor() {
+        if (this.filaLlamadas.length === 0) {
+            this.moviendose = false; // y esto para que sirve? porque andaba sin ponerlo.
+            return false;
+        }
+        this.moviendose = true; //Que alguien me explique porque ahora si funciona ???
+        var siguientePiso = this.filaLlamadas[0];
+        this.dibujarAscensor();
         this.filaLlamadas.splice(0, 1);
+
+        edificio.botonera.cambiarPantalla(siguientePiso);
+
+        var _this = this;
+        setTimeout(function() {
+            _this.moverAscensor()
+        }, 4000)
+    }
+
+    agregarPiso(piso){
+        if (this.filaLlamadas.length < 4 && !this.filaLlamadas.includes(piso)) {
+            this.filaLlamadas.push(piso);
+            if (!this.moviendose) {
+                this.moverAscensor();
+            }
+            return true;
+        }else{
+            return false;
+        }
+
     }
 }
